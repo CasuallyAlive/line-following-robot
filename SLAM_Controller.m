31b940a9ab559a052b78c3f6c00be6c7be8593d8
CasuallyAlive <jordy.larrearodriguez@gmail.com>
2022-03-28 14:50:07 -0600

    Updated SLAM_Controller class and began implementing working loop in file "traverser.m".

--- SLAM_Controller.m
+++ SLAM_Controller.m
@@ -1,4 +1,15 @@
 classdef SLAM_Controller
+    enumeration
+        StandBy, FollowLineForward, GraspItem, TurnAround, UnGraspItem, BeFree, Fork;
+    end
     properties
+        state;
+        classifier;
+    end
+    methods
+        function obj = SLAM_Controller(classifier)
+            obj.state = StandBy;
+            obj.classifier = classifier;
+        end
     end
 end 
