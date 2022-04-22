3f3d67f2f9a89e1f069938db643d014b70199759
CasuallyAlive <jordy.larrearodriguez@gmail.com>
2022-04-21 18:10:35 -0600

    Deployed a multi-class one-versus-one SVM classifier, with about 150 training examples. The result was a 90% accurate classifier on a small sub-set of the training set.

--- BlockClassifcationsDraft.m
+++ BlockClassifcationsDraft.m
@@ -34,7 +34,8 @@ first = true;
 countdown("Beginning Recording Bad blocks in", 3);
 for x = 1:trialCount
     fprintf("Please Place Bad Block, Trial No. %d\n", x);
-    pause(10);
+    while(input("Start recording\n","s") ~= "start")
+    end
     fprintf("reading data");
     [red, green, blue] = r.rgbRead();
     AnalogData = r.getAverageData('analog', 10);
@@ -147,7 +148,7 @@ clc; % clear command line
 if ~exist(saveDirectory, 'dir') %check if a data folder already exists
     mkdir(saveDirectory) %make folder if not
 end
-filename = ['data_', getenv('COMPUTERNAME'), '_', datestr(now,'yyyy-mm-dd-HH-MM-ss')];
+filename = ['data_2_', getenv('COMPUTERNAME'), '_', datestr(now,'yyyy-mm-dd-HH-MM-ss')];
 save(filename,'data') %save data
 
 % For Loading Data
@@ -223,24 +224,127 @@ f = @(x1, x2, x3) K + L(1)*x1 + L(2)*x2 +  L(3)*x3;
 h2 = fimplicit(f, limits);
 
 %% get raw data
-training_examples = zeros(5,75);
+
+training_examples_ideal = zeros(5,75);
+training_examples_unideal = zeros(5,75);
+
 i = 1;
 for row = 1:3
-    temp = cell2mat(data(1,:));
+    temp = cell2mat(data(row,:));
     for col = temp
-        training_examples(:,i) = col;
+        training_examples_ideal(:,i) = col;
         i = i+1;
     end
 end
 
-training_examples = training_examples(2:4,:);
-colors = (colors)./255;
-[coeff,score,latent,tsquared,explained,mu] = pca(colors');
+size_b = training_examples_ideal(5,:);
+training_examples_ideal(5,:) = (size_b - min(size_b))./(max(size_b) - min(size_b)); %normalize size
 
-Y = zeros(size(colors,2))
-Y()
+i = 1;
+for row = 1:3
+    temp = cell2mat(data_2(row,:));
+    for col = temp
+        training_examples_unideal(:,i) = col;
+        i = i+1;
+    end
+end
+
+training_examples = zeros(5,150);
+i = 1; j = 1;
+for col = 1:150
+    if(mod(col,2) == 0)
+        training_examples(:,col) = training_examples_unideal(:,i);
+        i = i+1;
+    else
+        training_examples(:,col) = training_examples_ideal(:,j);
+        j = j+1;
+    end
+end
+%% Normalize data
+colors = (training_examples_ideal(2:4,:));
+hall_effect = training_examples(1,:);
+
+training_examples(2:4,:) = training_examples(2:4,:)./255; %normalize colors
+training_examples(1,:) = (training_examples(1,:) - min(hall_effect))./(max(hall_effect) - min(hall_effect));
+
+colors = (training_examples(2:4,:));
+hall_effect = training_examples(1,:);
+size_b = training_examples(5,:);
+% coef = pca(colors);
+
+Y = zeros(1,150);
+Y(1:50) = 0; %% bad == 0
+Y(51:100) = 1; %% good == 1
+Y(101:150) = 2; %% excellent == 2
+
+%% Visualization red
+% scatter3(hall_effect(1:25), size(1:25), colors(1,1:25),50, "red", 'o') % bad
+% hold on
+% scatter3(hall_effect(26:50), size(26:50), colors(1,26:50),50, "blue", '+') % good
+% hold on
+% scatter3(hall_effect(51:75), size(51:75), colors(1,51:75),50, "green", '*') % excellent
+% hold on
+% % Visualization green
+% scatter3(hall_effect(1:25), size(1:25), colors(2,1:25),50, "red", 'o') % bad
+% hold on
+% scatter3(hall_effect(26:50), size(26:50), colors(2,26:50),50, "blue", '+') % good
+% hold on
+% scatter3(hall_effect(51:75), size(51:75), colors(2,51:75),50, "green", '*') % excellent
+% hold on
+% % Visualization blue
+% scatter3(hall_effect(1:25), size(1:25), colors(3,1:25),50, "red", 'o') % bad
+% hold on
+% scatter3(hall_effect(26:50), size(26:50), colors(3,26:50),50, "blue", '+') % good
+% hold on
+% scatter3(hall_effect(51:75), size(51:75), colors(3,51:75),50, "green", '*') % excellent
+
+%% pca 
+% scatter3(hall_effect(1:25), size(1:25), coef(1:25,1)',50, "red", 'o') % bad
+% hold on
+% scatter3(hall_effect(26:50), size(26:50), coef(26:50,1)',50, "blue", '+') % good
+% hold on
+% scatter3(hall_effect(51:75), size(51:75), coef(51:75,1)',50, "green", '*') % excellent
+
+%% separate into training and testing sets
+train_set = zeros(5,132);
+test_set = zeros(5,18);
+Y_train = zeros(1,132);
+Y_test = zeros(1,18);
+
+i = 1; j = 1;
+for cat = 1:3
+    offset = 44*(cat-1);
+    for col = 1:44
+        train_set(:,i) = training_examples(:,col + offset);
+        Y_train(:,i) = Y(:,col + offset);
+
+        i = i+1;
+    end
+    for col = 45:50
+        test_set(:,j) = training_examples(:, col + offset);
+        Y_test(:,j) = Y(:,col + offset);
+
+        j = j+1;
+    end
+end
+
+classes = unique(Y);
 %% SVM
+t = templateSVM('Standardize',true,'KernelFunction','polynomial');
 
+model = fitcecoc(train_set', Y_train,'Learners',t, 'FitPosterior',true,...
+    'ClassNames', classes);
+
+%% Test 
+correct = 0;
+for i = 1:18
+    result = model.predict(test_set(:,i)') == Y_test(i);
+    if(result == true)
+        correct = correct +1;
+    end
+end
+acc = round(correct./18*100);
+fprintf("Accuracy %d\n", acc);
 %% Claw Code Function
 function size = CloseClaw(obj)
 obj.servo(4,0);
